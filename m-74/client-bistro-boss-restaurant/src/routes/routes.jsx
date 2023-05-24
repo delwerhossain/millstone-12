e2b3a2363a5fce71cdb0eae4431032514b02f51f
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-24 06:27:05 +0600

    error page add

--- m-74/client-bistro-boss-restaurant/src/routes/routes.jsx
+++ m-74/client-bistro-boss-restaurant/src/routes/routes.jsx
@@ -1,16 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import ClientLayouts from "../Layout/ClientLayouts";
 import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <ClientLayouts></ClientLayouts>,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
         element: <Home></Home>,
       },
-    ]
+    ],
   },
 ]);
