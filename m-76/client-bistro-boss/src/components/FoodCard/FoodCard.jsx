3a442de87a9604222488f32a3f37e106946a348d
delwerhossain <delwerhossain006@gmail.com>
2023-05-30 11:36:57 +0100

    add to cart add

--- m-76/client-bistro-boss/src/components/FoodCard/FoodCard.jsx
+++ m-76/client-bistro-boss/src/components/FoodCard/FoodCard.jsx
@@ -1,20 +1,87 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import { useLocation, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import useCarts from "../../hooks/useCarts";
 
+const FoodCard = ({ item }) => {
+  // check user
+  const { user } = useContext(AuthContext);
+  // redirect part for
+  const navigate = useNavigate();
+  const location = useLocation();
+  // gobal query for rerendering
+  const [, refetch] = useCarts();
 
-const FoodCard = ({item}) => {
-    const {name, image, price, recipe} = item;
-    return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={image} alt="Shoes" /></figure>
-            <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
-            <div className="card-body flex flex-col items-center">
-                <h2 className="card-title">{name}</h2>
-                <p>{recipe}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
-                </div>
-            </div>
+  // cart item details
+  const { _id, name, image, price, recipe } = item;
+  // cart handle
+  const handleCarts = () => {
+    if (user && user.email) {
+      const cartItem = {
+        menuItemId: _id,
+        name,
+        image,
+        price,
+        email: user.email,
+      };
+      fetch("http://localhost:5000/carts", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(cartItem),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.insertedId) {
+            refetch(); // refetch cart to update the number of items in the cart
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Food added on the cart.",
+              showConfirmButton: false,
+              timer: 500,
+            });
+          }
+        });
+    } else {
+      Swal.fire({
+        title: "Please login to order the food",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login now!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location } });
+        }
+      });
+    }
+  };
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={image} alt="Shoes" />
+      </figure>
+      <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">
+        ${price}
+      </p>
+      <div className="card-body flex flex-col items-center">
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
+        <div className="card-actions justify-end">
+          <button
+            onClick={handleCarts}
+            className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4"
+          >
+            Add to Cart
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FoodCard;
