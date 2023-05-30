bcbb14b42f17ac6b4de04bb8ea56048c26a6e282
delwerhossain <delwerhossain006@gmail.com>
2023-05-30 15:26:44 +0100

    react icon and  dash add

---
+++ m-76/client-bistro-boss/src/Layout/Dashboard.jsx
@@ -0,0 +1,68 @@
+import { NavLink, Outlet } from "react-router-dom";
+import {
+  FaShoppingCart,
+  FaWallet,
+  FaCalendarAlt,
+  FaHome,
+} from "react-icons/fa";
+import useCarts from "../hooks/useCarts";
+
+const Dashboard = () => {
+  const [cart] = useCarts();
+  return (
+    <div className="drawer drawer-mobile ">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        <Outlet></Outlet>
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side bg-[#D1A054]">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80">
+          <li>
+            <NavLink to="/dashboard/home">
+              <FaHome></FaHome> User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/reservations">
+              <FaCalendarAlt></FaCalendarAlt> Reservations
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/history">
+              <FaWallet></FaWallet> Payment History
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/mycart">
+              <FaShoppingCart></FaShoppingCart> My Cart
+              <span className="badge inl badge-secondary">
+                +{cart?.length || 0}
+              </span>
+            </NavLink>
+          </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome></FaHome> Home
+            </NavLink>{" "}
+          </li>
+          <li>
+            <NavLink to="/menu"> Our Menu</NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">Order Food</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
