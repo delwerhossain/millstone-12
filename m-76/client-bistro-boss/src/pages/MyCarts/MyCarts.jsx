01d0b0bf4d169553f58e918589e2a4531e8d5566
Delwer Hossain <delwerhossain006@gmail.com>
2023-06-01 06:16:38 +0600

    my cart table add and delete fun ad

--- m-76/client-bistro-boss/src/pages/MyCarts/MyCarts.jsx
+++ m-76/client-bistro-boss/src/pages/MyCarts/MyCarts.jsx
@@ -1,9 +1,92 @@
+import useCarts from "../../hooks/useCarts";
+import { Helmet } from "react-helmet-async";
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+
 const MyCarts = () => {
-    return (
-        <div>
-            <h1 className='text-5xl'> this My carts 1</h1>
-        </div>
-    );
+  const [cart, refetch] = useCarts();
+  const total = cart.reduce(
+    (accumulator, currentVal) => accumulator + currentVal.price,
+    0
+  );
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/carts/${item._id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+
+  return (
+    <div className="w-full ">
+      <Helmet>
+        <title>Bistro Boss | My Cart</title>
+      </Helmet>
+      <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
+        <h3 className="text-3xl">Total Items: {cart.length}</h3>
+        <h3 className="text-3xl">Total Price: ${total}</h3>
+        <button className="btn btn-warning btn-sm">PAY</button>
+      </div>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Food</th>
+              <th>Item Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={item.image}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{item.name}</td>
+                <td className="text-end">${item.price}</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(item)}
+                    className="btn btn-ghost bg-red-600  text-white"
+                  >
+                    <FaTrashAlt></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyCarts;
