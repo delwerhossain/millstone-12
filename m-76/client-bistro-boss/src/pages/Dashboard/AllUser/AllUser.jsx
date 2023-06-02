4612e02610c6927204bb19a8406c35b0d2737ee4
Delwer Hossain <delwerhossain006@gmail.com>
2023-06-02 08:44:15 +0600

    table user add

--- m-76/client-bistro-boss/src/pages/Dashboard/AllUser/AllUser.jsx
+++ m-76/client-bistro-boss/src/pages/Dashboard/AllUser/AllUser.jsx
@@ -1,16 +1,71 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
+
 
 const AllUser = () => {
-const { refetch, data: users = [] } = useQuery({
-  queryKey: ["users"],
-  queryFn: async () => {
-    const res = await fetch("http://localhost:5000/users");
-    return res.json();
-  },
-});
+  const { refetch, data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/users");
+      return res.json();
+    },
+  });
 
-    return <div>user --- {users.length}</div>;
+  return (
+    <div className="w-full">
+      <Helmet>
+        <title>Bistro Boss | All users</title>
+      </Helmet>
+      <h3 className="text-3xl font-semibold my-4">
+        Total Users: {users.length}
+      </h3>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.role === "admin" ? (
+                    "admin"
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn btn-ghost bg-orange-600  text-white"
+                    >
+                      <FaUserShield></FaUserShield>
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user)}
+                    className="btn btn-ghost bg-red-600  text-white"
+                  >
+                    <FaTrashAlt></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUser;
