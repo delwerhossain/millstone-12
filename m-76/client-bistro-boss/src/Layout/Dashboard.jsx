8f1a1e8b79fcaf43efd34ecb609080d627dc363f
Delwer Hossain <delwerhossain006@gmail.com>
2023-06-02 08:15:32 +0600

    admin lay outs set

--- m-76/client-bistro-boss/src/Layout/Dashboard.jsx
+++ m-76/client-bistro-boss/src/Layout/Dashboard.jsx
@@ -4,11 +4,19 @@ import {
   FaWallet,
   FaCalendarAlt,
   FaHome,
+  FaUtensils,
+  FaBook,
+  FaUsers,
 } from "react-icons/fa";
 import useCarts from "../hooks/useCarts";
 
 const Dashboard = () => {
   const [cart] = useCarts();
+
+  // TODO: load data from the server to have dynamic isAdmin based on Data
+  // const isAdmin = true;
+  const isAdmin = true;
+
   return (
     <div className="drawer drawer-mobile ">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -24,29 +32,63 @@ const Dashboard = () => {
       <div className="drawer-side bg-[#D1A054]">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80">
-          <li>
-            <NavLink to="/dashboard/home">
-              <FaHome></FaHome> User Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/reservations">
-              <FaCalendarAlt></FaCalendarAlt> Reservations
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/history">
-              <FaWallet></FaWallet> Payment History
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/mycart">
-              <FaShoppingCart></FaShoppingCart> My Cart
-              <span className="badge inl badge-secondary">
-                +{cart?.length || 0}
-              </span>
-            </NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to="/dashboard/home">
+                  <FaHome></FaHome> Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservations">
+                  {" "}
+                  <FaUtensils></FaUtensils> Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/history">
+                  <FaWallet></FaWallet> Manage Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/history">
+                  <FaBook></FaBook> Manage Bookings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/allusers">
+                  <FaUsers></FaUsers> All Users
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard/home">
+                  <FaHome></FaHome> User Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservations">
+                  <FaCalendarAlt></FaCalendarAlt> Reservations
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/history">
+                  <FaWallet></FaWallet> Payment History
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/mycart">
+                  <FaShoppingCart></FaShoppingCart> My Cart
+                  <span className="badge inl badge-secondary">
+                    +{cart?.length || 0}
+                  </span>
+                </NavLink>
+              </li>
+            </>
+          )}
+
           <div className="divider"></div>
           <li>
             <NavLink to="/">
