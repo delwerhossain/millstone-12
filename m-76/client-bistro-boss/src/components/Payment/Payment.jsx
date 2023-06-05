7affa53a172610958dfac1926111a6b168742c31
Delwer Hossain <delwerhossain006@gmail.com>
2023-06-05 06:47:42 +0600

    card short validation and test

--- m-76/client-bistro-boss/src/components/Payment/Payment.jsx
+++ m-76/client-bistro-boss/src/components/Payment/Payment.jsx
@@ -1,51 +1,70 @@
-import {
-  CardElement,
-  useStripe,
-  useElements,
-} from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const Payment = () => {
   const stripe = useStripe();
   const elements = useElements();
 
+  const [cardError, setCardError] = useState("");
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    if (elements == null) {
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
       return;
     }
 
-    // const { error, paymentMethod } = await stripe.createPaymentMethod({
-    //   type: "card",
-    //   card: elements.getElement(CardElement),
-    // });
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    // Use your card Element with other Stripe.js APIs
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+        console.log("[error]", error);
+        setCardError(error.message);
+    } else {
+        console.log("[PaymentMethod]", paymentMethod);
+        setCardError('');
+    }
   };
+
   return (
-    <form className="w-2/3 m-8" onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <>
+      <form className="w-2/3 m-8" onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button
-        className="btn btn-primary btn-sm mt-4"
-        type="submit"
-        disabled={!stripe}
-      >
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <button
+          className="btn btn-primary btn-sm mt-4"
+          type="submit"
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && <p className="font-semibold text-red-600 ml-8">{cardError}</p>}
+    </>
   );
 };
 
